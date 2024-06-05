b6058506d2fe19cbd3a2d776534702ad4c917113
rikarani <46773591+rikarani@users.noreply.github.com>
2024-06-06 03:02:29 +0700

    chore: Update navbar styling and remove unused code

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,20 +1,7 @@
-import Show from "@/components/show";
-import Link from "next/link";
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-
 export default function Navbar(): React.JSX.Element {
   return (
     <nav className="sticky top-0 z-50 w-full bg-background/70 backdrop-blur-md">
-      <div className="px-6 py-3">
+      <div className="px-6 py-4">
         <div className={`flex items-center justify-between py-0`}>
           <div className="flex items-center justify-start">
             <a href="/" className="flex md:mr-24">
@@ -22,40 +9,6 @@ export default function Navbar(): React.JSX.Element {
                 Erikacang
               </span>
             </a>
-          </div>
-          <div className="flex items-center">
-            <div className="ms-3 flex items-center">
-              <Show>
-                <Show.When isTrue={true}>
-                  <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                      <Avatar className="hover:cursor-pointer">
-                        <AvatarImage src="https://github.com/rikarani.png" />
-                      </Avatar>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent
-                      align="end"
-                      className="border-gray-800 bg-gray-950 text-white"
-                    >
-                      <DropdownMenuLabel>Phasya Ananta</DropdownMenuLabel>
-                      <DropdownMenuLabel className="text-xs text-gray-400">
-                        rikarani
-                      </DropdownMenuLabel>
-                      <DropdownMenuSeparator className="bg-gray-800" />
-                      <DropdownMenuGroup>
-                        <DropdownMenuItem className="hover:cursor-pointer focus:bg-[hsl(240_3.7%_15.9%)] focus:text-white">
-                          <Link href="/dashboard">Dashboard</Link>
-                        </DropdownMenuItem>
-                      </DropdownMenuGroup>
-                      <DropdownMenuSeparator className="bg-gray-800" />
-                      <DropdownMenuItem className="hover:cursor-pointer focus:bg-[hsl(240_3.7%_15.9%)] focus:text-white">
-                        <Link href="/dashboard">Dashboard</Link>
-                      </DropdownMenuItem>
-                    </DropdownMenuContent>
-                  </DropdownMenu>
-                </Show.When>
-              </Show>
-            </div>
           </div>
         </div>
       </div>
