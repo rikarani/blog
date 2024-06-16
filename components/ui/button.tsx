52d517a03792908857b96339fa3be831e82a12f6
rikarani <46773591+rikarani@users.noreply.github.com>
2024-06-17 01:02:04 +0700

    refactor: Update form components to disable inputs during form submission

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import * as React from "react";
+import { useFormStatus } from "react-dom";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
@@ -38,9 +41,16 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const { pending } = useFormStatus();
+
     const Comp = asChild ? Slot : "button";
     return (
-      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+      <Comp
+        disabled={pending}
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
     );
   },
 );
